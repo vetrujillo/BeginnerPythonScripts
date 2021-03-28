4b49cde5489df27b15aff12452359fe9aede9026
Victor Trujillo <49459459+vetrujillo@users.noreply.github.com>
2021-03-28 11:54:54 -0700

    Add files via upload
    Read in each line from the file you just created, learning_python.txt, and replace the word Python with the name of another language, such as C . Print each modified line to the screen .

---
+++ ex_file_modify.py
@@ -0,0 +1,26 @@
+filename = 'learning_python.txt'
+
+with open(filename) as file_object:
+    contents = file_object.read()
+    print(contents)
+
+print("\n")
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+    list = []
+    for line in lines:
+        print(line.rstrip())
+        list.append(line)
+
+print("\n")
+
+for item in list:
+    print(item.rstrip())
+
+print("\n")
+
+for change in list:
+    print(change.replace('Python', 'Ruby').rstrip())
+    
+    
